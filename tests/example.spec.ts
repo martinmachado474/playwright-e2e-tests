6b0ca97a5286f35834d06abc5be1f2a7df11dfed
martin <martinmachado474@gmail.com>
2024-12-26 20:37:44 -0300

    TST-01 start login feature

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('go to page demoblaze', async ({ page }) => {
+  await page.goto('https://www.demoblaze.com/');
+ 
+});
+test('go to page orangeHR', async ({ page }) => {
+  await page.goto('https://www.orangehrm.com/');
+  await page.waitForSelector('#navbarSupportedContent');
+  await page.click('#navbarSupportedContent .nav-link-hed >> text=Pricing');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
